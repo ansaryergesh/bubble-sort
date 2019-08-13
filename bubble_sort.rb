0bb3e2cc4c8813ce34808fe93600c04979108cd8
ansaryergesh <yergeshovansar@gmail.com>
2019-08-14 02:24:06 +0600

    created bubble_sort_by function

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -22,3 +22,32 @@ def bubble__sort(arr)
     arr
 end
 puts bubble__sort(ArrayOfNumbers);
+
+# Bubble Sort By function with block
+puts "Bubble_sort_by function by using yield"
+
+def bubble__sort_by(arr)
+  n = arr.length;
+  flag = false;
+  for j in 0..n-1
+      if (!flag&&(j!=0))
+          break;
+      end
+      flag = false;
+      for i in 1..n-1
+          if yield(arr[i-1],arr[i]) > 0 
+              help = arr[i-1];
+              arr[i-1] = arr[i];
+              arr[i] = help;
+              flag = true;
+          end
+      end
+  end
+  arr
+end
+
+result = bubble__sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+    end
+
+puts result.join(",")
